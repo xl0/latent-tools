430fa41b07d5e6c6977c02a5006218ea201a21cb
Alexey <alexey.zaytsev@gmail.com>
2025-05-19 04:51:22 -0300

    Add a node to concatentte latent and a node to set image size in one place

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
 from .load_latent import QLoadLatent, QLoadLatentTimeline
+from .concat_latent_batch import QConcatLatentBatch
 from .schedulers import QLinearScheduler
 # from .model_metadata_preview import PreviewModelMetadata
 from .preview_latent import QPreviewLatent
 from .generate_latent_gaussian import QGaussianLatent
 from .generate_latent_uniform import QUniformLatent
 from .samplers import QKSampler, QSamplerCustom, QSamplerEulerAncestral
+from .image_size_setter import QImageSizeSetter
 
-import lovely_tensors as lt; lt.monkey_patch()
+# import lovely_tensors as lt; lt.monkey_patch()
 
 NODE_CLASS_MAPPINGS = {
     "QLoadLatent": QLoadLatent,
     "QLoadLatentTimeline": QLoadLatentTimeline,
+    "QConcatLatentBatch": QConcatLatentBatch,
     "QLinearScheduler": QLinearScheduler,
     # "PreviewModelMetadata": PreviewModelMetadata,
     "QPreviewLatent": QPreviewLatent,
@@ -18,7 +21,8 @@ NODE_CLASS_MAPPINGS = {
     "QUniformLatent": QUniformLatent,
     "QKSampler": QKSampler,
     "QSamplerCustom": QSamplerCustom,
-    "QSamplerEulerAncestral": QSamplerEulerAncestral
+    "QSamplerEulerAncestral": QSamplerEulerAncestral,
+    "QImageSizeSetter": QImageSizeSetter
 }
 
 WEB_DIRECTORY="./web/js"
---
+++ concat_latent_batch.py
@@ -0,0 +1,31 @@
+import torch
+import lovely_tensors as lt
+
+class QConcatLatentBatch:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "latent1": ("LATENT", {}),
+                "latent2": ("LATENT", {}),
+            }
+        }
+
+    CATEGORY = "QTools"
+    FUNCTION = "concat"
+    RETURN_TYPES = ("LATENT", )
+
+    def concat(self, latent1: dict, latent2: dict):
+        assert isinstance(latent1, dict), f"Incorrect type for latent1: Expected dict, got {type(latent1)}"
+        assert isinstance(latent2, dict), f"Incorrect type for latent2: Expected dict, got {type(latent2)}"
+        samples1 = latent1["samples"]
+        samples2 = latent2["samples"]
+        assert isinstance(samples1, torch.Tensor), f"Incorrect type for latent1.samplels: Expected torch.Tensor, got {type(samples1)}"
+        assert isinstance(samples2, torch.Tensor), f"Incorrect type for latent2.samplels: Expected torch.Tensor, got {type(samples2)}"
+
+        if samples1.dim() != 4: raise ValueError(f"latent1 should have 4 dimensions, got {lt.lovely(samples1)}")
+        if samples2.dim() != 4: raise ValueError(f"latent2 should have 4 dimensions, got {lt.lovely(samples2)}")
+
+        concatenated = torch.cat([samples1, samples2], dim=0)
+
+        return ({"samples": concatenated},)
